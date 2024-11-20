faf210ec319e26aa001065f4a9dbf1a9b4518071
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-11-20 13:18:49 +0100

    initial commit

---
+++ controllers/postsController.js
@@ -0,0 +1,27 @@
+const postList = require("../data/posts.js");
+
+function index(postList) {
+
+}
+
+function show(postList) {
+
+}
+
+function store(postList) {
+
+}
+
+function update(postList) {
+
+}
+
+function modify(postList) {
+
+}
+
+function destroy(postList) {
+
+}
+
+module.exports(index, show, store, update, modify, destroy);
---
+++ routers/postsRouter.js
@@ -0,0 +1,1 @@
+const functions = require("../controllers/postsController.js");
